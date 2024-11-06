3da647f256d21d3a7c2912d078d8ce10a402f4cf
girvel <widauka@ya.ru>
2024-11-06 13:55:16 +0500

    Colored smoke

--- smoke/main.lua
+++ smoke/main.lua
@@ -1,3 +1,5 @@
+love.graphics.setDefaultFilter("nearest", "nearest")
+
 local shader_source = love.filesystem.read("shader.glsl")
 local smoke_shader = love.graphics.newShader(shader_source)
 
@@ -7,17 +9,25 @@ local canvases = {
 }
 
 love.graphics.setCanvas(canvases.current)
-love.graphics.circle("fill", 100, 400, 50)
+-- love.graphics.circle("fill", 100, 400, 50)
+love.graphics.draw(
+  love.graphics.newImage("decorations.png"),
+  0, 0,
+  0,
+  4
+)
 love.graphics.setCanvas()
 
 function love.draw()
   -- shaders step --
-  love.graphics.setCanvas(canvases.next)
-  love.graphics.setShader(smoke_shader)
-  love.graphics.draw(canvases.current)
-  love.graphics.setCanvas()
-  love.graphics.setShader()
-  canvases.current, canvases.next = canvases.next, canvases.current
+  if love.keyboard.isDown("a") then
+    love.graphics.setCanvas(canvases.next)
+    love.graphics.setShader(smoke_shader)
+    love.graphics.draw(canvases.current)
+    love.graphics.setCanvas()
+    love.graphics.setShader()
+    canvases.current, canvases.next = canvases.next, canvases.current
+  end
 
   -- actually display --
   love.graphics.draw(canvases.current)
--- smoke/shader.glsl
+++ smoke/shader.glsl
@@ -1,23 +1,16 @@
+const int SCALE = 4;
+
+vec2 right = vec2(SCALE / love_ScreenSize.x, 0.);
+vec2 down = vec2(0., SCALE / love_ScreenSize.y);
+vec2 left = -right;
+vec2 up = -down;
+
 vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
 {
-    // TODO! move out
-    vec2 right = vec2(1 / love_ScreenSize.x, 0.);
-    vec2 down = vec2(0., 1 / love_ScreenSize.y);
-    vec2 left = -right;
-    vec2 up = -down;
-
-    vec4 pixel = Texel(tex, texture_coords);
-    
-    vec4 up_pixel = Texel(tex, texture_coords + up);
-    vec4 left_pixel = Texel(tex, texture_coords + left);
-    vec4 right_pixel = Texel(tex, texture_coords + right);
-    vec4 down_pixel = Texel(tex, texture_coords + down);
-
-    return pixel + (
-        - 6 * pixel
-        + up_pixel
-        + left_pixel 
-        + right_pixel
-        + 3 * down_pixel
-    ) / 6;
+    return vec4((
+        + Texel(tex, texture_coords + up)
+        + Texel(tex, texture_coords + left)
+        + Texel(tex, texture_coords + right)
+        + 3 * Texel(tex, texture_coords + down)
+    ).rgb / 6, 1.);
 }
